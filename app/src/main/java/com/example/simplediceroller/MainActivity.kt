6327a5f218ecea46e6a8b547a5180182f0fd4f15
DT5Gen <alex-shilov@yandex.ru>
2022-12-13 02:50:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/simplediceroller/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.simplediceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simplediceroller.ui.theme.SimpleDiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleDiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+
+@Composable
+
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+}
